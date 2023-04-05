59511a7f39dd6f80d1c7c7311238ec19411967b1
riya <79600393+riuu124421@users.noreply.github.com>
2023-04-05 16:19:13 +0530

    Add files via upload

---
+++ QuizApp/js/questions.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+
+    {
+     numb: 6,
+     question: "In CSS, h1 can be called as?",
+     answer: "Selector",
+     options: [
+       " Selector",
+       "Value",
+       "Attribute",
+       "Tag"
+     ]
+   },
+   {
+    numb: 7,
+    question: "Which HTML tag is used to define an internal style sheet?",
+    answer: " style",
+    options: [
+        "css",
+        "text/style",
+        "style",
+        "script"
+    ]
+   },
+   {
+    numb: 8,
+    question: "CSS padding property is used for?",
+    answer: "Space",
+    options: [
+        " Border",
+        "Space",
+        "Margin",
+        "Background color"
+    ]
+   },
+   {
+    numb: 9,
+    question: "Which is the correct CSS syntax?",
+    answer: "body {color: black}",
+    options: [
+        "body {color: black}",
+        "{body;color:black}",
+        "{body:color=black(body}",
+        "body:color=black",
+    ]
+   },
+   {
+    numb: 10,
+    question: " In CSS, “font-size” can be called as?",
+    answer: " Property-Name",
+    options: [
+        " Selector",
+        "Property",
+        "Rule",
+        "Property-Name"
+    ]
+   }
+];
